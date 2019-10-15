a407d3383b5e0adc723dc54cc7e216491937ea42
Maikaru <maikaru@maikaru.com>
2019-10-15 22:39:22 +0000

    Done.

---
+++ display_board.rb
@@ -0,0 +1,12 @@
+# Define display_board that accepts a board and prints
+# out the current state.
+def display_board
+ def say_display_board_ten_times
+  phrase1 = "   |   |   "
+  phrase2 = "-----------"
+  puts phrase1
+  puts phrase2
+  puts phrase1
+  puts phrase2
+  puts phrase1
+ end
